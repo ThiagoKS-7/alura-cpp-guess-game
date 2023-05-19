36a86c6a6803b236d7a9847c4ef802eb2949fd21
Thiago Kasper de Souza <thiagokasper101@gmail.com>
2023-05-19 17:20:28 -0300

    Implementing score system

--- game.cpp
+++ game.cpp
@@ -8,13 +8,15 @@ void println(string value) {
 namespace Game {
     class GuessGame {
         public:
-            int secret_num = 32;
+            const int SECRET_NUM = 32;
             int attempts = 0;
+            double score = 1000.0;
             bool debug = true;
             bool found_secret = false;
+
             void play() {
                 if (debug) {
-                    cout << "Secret number is : " << secret_num << endl;
+                    cout << "Secret number is : " << SECRET_NUM << endl;
                 }
                 println("******************************");
                 println("*  Welcome to Guessing Game  *");
@@ -22,6 +24,10 @@ namespace Game {
                 while (!found_secret) {
                     ask_guess();
                 }
+            }
+            double calculate_penalty(int guess) {
+                double penalty = abs((double)(SECRET_NUM - guess)/2);
+                return penalty > 0 ? penalty : 1;
             }
         private:
             void ask_guess() {
@@ -36,17 +42,22 @@ namespace Game {
             }
             void validate_guess(int current_guess) {
                 attempts ++;
-                if (current_guess > secret_num)
+                score = score -  calculate_penalty(current_guess);
+                cout << score;
+                if (current_guess > SECRET_NUM)
+                {   
+                    inform_response("You guessed too high! try again\n");
+                } else if (current_guess < SECRET_NUM)
                 {
-                    inform_response("You guessed to  high! try again\n");
-                } else if (current_guess < secret_num)
-                {
-                    inform_response("You guessed to  low! try again\n");
+                    inform_response("You guessed too low! try again\n");
                 } else 
                 {
                     found_secret = true;
                     inform_response("You guessed right,congrats!\n");
-                    cout << "FINAL SCORE: " << 123 << endl;
+                    cout.precision(2);
+                    //precisa explicitar pra não sair em notação científica
+                    cout << fixed; 
+                    cout << "FINAL SCORE: " << score << endl;
                 }
                 
             }
